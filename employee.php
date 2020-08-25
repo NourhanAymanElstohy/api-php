73bd2a0b4fdd4b4e416aa99db6fc5c9991dfcdc2
nour <nour7anelsto7y@gmail.com>
2020-08-25 14:09:52 +0200

    add get by id request

--- employee.php
+++ employee.php
@@ -21,6 +21,22 @@ class Employee
 
         return $stmt;
     }
+    public function read_single () 
+    {
+        $query = "SELECT e.id, e.name, e.phone, e.age from $this->table_name e where e.id=?";
+        $stmt = $this->conn->prepare($query);
+
+        $stmt->bindParam(1, $this->id);
+        $stmt->execute();
+
+        $row = $stmt->fetch(PDO::FETCH_ASSOC);
+
+        $this->name = $row['name'];
+        $this->phone = $row['phone'];
+        $this->age = $row['age'];
+
+        // return $stmt;
+    }
     public function create()
     {
         $query = "INSERT INTO " . $this->table_name .
